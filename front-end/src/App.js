e731535b873b247087fa92afac56683eee10892b
YOON-CC <whdbscks77@gmail.com>
2023-05-27 05:12:49 +0900

    feat : 메인페이지 레이아웃 완료, 스크롤 적용시 게시글이 스크롤 Y좌표에 따라 계속 렌더링 완료

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Home_header from "./component/header/home_header";
+import Home_board_list from "./component/body/home_board_list";
+
 
 export default class App extends Component{
   render() { 
   return (
     <div className="App">
       <Home_header></Home_header>
+      <Home_board_list></Home_board_list>
     </div>
   );
 }
---
+++ front-end/src/component/body/home_board_list.jsx
@@ -0,0 +1,73 @@
+import React, { Component, useEffect, useState} from "react";
+import "./home_board_list.css";
+
+
+
+export default class Home_board_list extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            boards: [],
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 100) {
+            const newBoard = <div key={scrollPosition} className="board-list">board {scrollPosition}</div>;
+            this.setState(prevState => ({ boards: [...prevState.boards, newBoard] }));
+        }
+    };
+    
+    
+    render() { 
+        const { boards } = this.state;
+
+        const BoardRows = [];
+        for (let i = 0; i < boards.length; i += 3) {
+          const row = boards.slice(i, i + 3);
+          const tempboard = <div key={i} className="board-row">{row}</div>;
+          BoardRows.push(tempboard);
+        }
+
+        return (
+            <div className="home_board_list_body">
+
+                <div className="home_hashtag_body">
+                    <div className="home_hashtag_container">
+                        <div className="home_hashtag_container_c1">전체</div>
+                        <div className="home_hashtag_container_c2">Q&A</div>
+                        <div className="home_hashtag_container_c3">카테고리</div>
+                        <div className="home_hashtag_container_c4">카테고리 저장</div>
+                    </div>
+                </div>
+                
+                <div className="board-list-container">
+                    <div key={0} className="board-list">board {0}</div>
+                    <div key={1} className="board-list">board {1}</div>
+                    <div key={2} className="board-list">board {2}</div>
+                    <div key={3} className="board-list">board {3}</div>
+                    <div key={4} className="board-list">board {4}</div>
+                    <div key={5} className="board-list">board {5}</div>
+                    <div key={6} className="board-list">board {6}</div>
+                    <div key={7} className="board-list">board {7}</div>
+                    <div key={8} className="board-list">board {8}</div>
+                    <div key={9} className="board-list">board {9}</div>
+                    <div key={10} className="board-list">board {10}</div>
+                    <div key={11} className="board-list">board {11}</div>
+                    {BoardRows}
+                </div>
+                
+            </div>
+        )
+    }
+}
